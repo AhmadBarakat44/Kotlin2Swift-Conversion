bf6dcccfa77fbe825df2eb38804442033c2a6515
Ahmad Barakat <77456753+AhmadBarakat44@users.noreply.github.com>
2022-12-01 20:43:55 +0200

    updates to the visitor , solved null issue
    visitor class now visits all objects under the top level, and all class members
    added a 2nd simpler test case for brevity

--- KotlinVisitor.java
+++ KotlinVisitor.java
@@ -1,70 +1,104 @@
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 
-public class KotlinVisitor extends KotlinParserBaseVisitor<String> {
-    //Importing kotlin lines
+public class KotlinVisitor extends KotlinParserBaseVisitor<String>
+{
     @Override
     public String visitKotlinFile(KotlinParser.KotlinFileContext ctx) {
-        return super.visitKotlinFile(ctx);
+        String toplevelobjs = "";
+        for (KotlinParser.TopLevelObjectContext ct : ctx.topLevelObject())
+        {
+            toplevelobjs += (visitTopLevelObject(ct));
+        }
+        return (visitPackageHeader(ctx.packageHeader()) + visitImportList(ctx.importList()) +toplevelobjs);
+    }
+    @Override
+    public String visitPackageHeader(KotlinParser.PackageHeaderContext ctx) {
+        //return super.visitPackageHeader(ctx);
+        return "no package header yet\n";
     }
 
-    //Changing Function declaration
+    @Override
+    public String visitImportList(KotlinParser.ImportListContext ctx) {
+        //return super.visitImportList(ctx);
+        return "no imports yet\n";
+    }
+
+    @Override
+    public String visitTopLevelObject(KotlinParser.TopLevelObjectContext ctx) {
+        return (visitDeclaration(ctx.declaration()) + visitSemis(ctx.semis()));
+    }
+
     @Override
     public String visitFunctionDeclaration(KotlinParser.FunctionDeclarationContext ctx) {
-        return ("func " + visitSimpleIdentifier(ctx.simpleIdentifier())
+       return ("func " + visitSimpleIdentifier(ctx.simpleIdentifier())
                 + visitFunctionValueParameters(ctx.functionValueParameters())
                 + "\n" + visitFunctionBody(ctx.functionBody()));
     }
 
-    // Getting values of the parameters of the function
     @Override
     public String visitFunctionValueParameters(KotlinParser.FunctionValueParametersContext ctx) {
         return "()";
     }
 
-    //Get the content of the function body
+
     @Override
     public String visitBlock(KotlinParser.BlockContext ctx) {
-        return ("{\n" + visitStatements(ctx.statements()) + "\n}");
+        return ("{\n" + visitStatements(ctx.statements()) + "\n}\n");
     }
 
-    // Make statements of the function
-    @Override
+   /* @Override
     public String visitStatements(KotlinParser.StatementsContext ctx) {
-        //todo implement statements
-        return "no statement yet";
+        String statements = "";
+        for (KotlinParser.StatementContext ct : ctx.statement())
+        {
+            statements += (visitStatement(ct));
+        }
+        return statements;
     }
 
-    //Get class declaration
+    @Override
+    public String visitStatement(KotlinParser.StatementContext ctx) {
+        return super.visitStatement(ctx);
+    }*/
+
     @Override
     public String visitClassDeclaration(KotlinParser.ClassDeclarationContext ctx) {
         return ("class " + visitSimpleIdentifier(ctx.simpleIdentifier()) + "\n" + visitClassBody(ctx.classBody()));
     }
 
-    // Get the content of the class body
     @Override
     public String visitClassBody(KotlinParser.ClassBodyContext ctx) {
         return ("{\n" + visitClassMemberDeclarations(ctx.classMemberDeclarations()) + "\n}");
     }
 
-    //Get the type of the variables declared
     @Override
-    public String visitPropertyDeclaration(KotlinParser.PropertyDeclarationContext ctx) {
-        return ("var" + visitVariableDeclaration(ctx.variableDeclaration()) + "="
-                + visitExpression(ctx.expression()) + "\n");
+    public String visitClassMemberDeclarations(KotlinParser.ClassMemberDeclarationsContext ctx) {
+        String myout = "";
+        for (KotlinParser.ClassMemberDeclarationContext ct : ctx.classMemberDeclaration())
+        {
+           myout += (visitClassMemberDeclaration(ct));
+        }
+        return myout;
     }
 
-    //Transform constant
+    @Override
+    public String visitSemis(KotlinParser.SemisContext ctx) {
+        return "\n";
+    }
+
+    @Override
+    public String visitPropertyDeclaration(KotlinParser.PropertyDeclarationContext ctx) {
+        return ("var " + visitVariableDeclaration(ctx.variableDeclaration()) + "= "
+                + visitExpression(ctx.expression()) +"\n");
+    }
+
     @Override
     public String visitLiteralConstant(KotlinParser.LiteralConstantContext ctx) {
         return (ctx.IntegerLiteral().getText());
     }
 
-    //
     @Override
     public String visitSimpleIdentifier(KotlinParser.SimpleIdentifierContext ctx) {
         return ctx.Identifier().getText() + " ";
     }
-    @Override
-    public String visitScript(KotlinParser.ScriptContext ctx) {
-        return visitChildren(ctx); }
 }
--- text.kt
+++ text.kt
@@ -1,7 +1,21 @@
 class s
 {
-    val x = 5
+    var x = 5
+    var y = 9
     fun main()
+    {
+
+    }
+    fun test()
+    {
+
+    }
+}
+class w
+{
+    var t = 0
+    var z = 0
+    fun myfun()
     {
 
     }
